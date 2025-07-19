944762d808cb59c8b9f55fe9c2a768272bacb057
emkan74 <143725894+emkan74@users.noreply.github.com>
2025-07-19 20:16:12 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات من آنلاین است ✅")
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token("توکن_تو").build()
+    app = ApplicationBuilder().token("7710339310:AAHYUgVvbUPkUPYBPSSGGw5pYdIOqcIDr_g").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
