81850e62e8d460e634f40ba7648881b8aed6dc8e
emkan74 <143725894+emkan74@users.noreply.github.com>
2025-07-19 20:15:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
-
-TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! ربات من روی Render اجرا می‌شه ✅")
+    await update.message.reply_text("سلام! ربات من آنلاین است ✅")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token("توکن_تو").build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
